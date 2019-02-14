77bc197d4d7e2f561bdf2837a1cea4a3e7fd5712
Luca Ventura <lventura@forcura.com>
2019-02-14 09:37:20 -0500

    adding IME_ACTION_DONE to pininputview edit text

--- applock/src/main/java/com/guardanis/applock/pin/PINInputView.java
+++ applock/src/main/java/com/guardanis/applock/pin/PINInputView.java
@@ -72,8 +72,13 @@ public class PINInputView extends LinearLayout implements TextWatcher {
         editText.setTextColor(getResources().getColor(android.R.color.transparent));
         editText.setCursorVisible(false);
         editText.setInputType(InputType.TYPE_CLASS_NUMBER);
-        editText.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI);
+        editText.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI|EditorInfo.IME_ACTION_DONE);
         editText.addTextChangedListener(this);
+
+
+        //editText.setHeight(1);
+        //editText.setWidth(1);
+
 
         addView(editText);
     }
